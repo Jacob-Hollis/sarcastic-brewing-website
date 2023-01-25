2b2c7c8a7cd7af357409ad72bba0544a75609f96
Jacob Hollis <jbhollis@pm.me>
2023-01-25 15:23:56 -0600

    Header finished, aside from links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './images/logowithname.png';
+import logo from './images/logo.png';
+import iglogo from './images/iglogo.png';
+import tiktoklogo from './images/tiktoklogo.png';
 import './App.css';
 import React from "react";
 import Header from "./Header";
@@ -8,20 +10,35 @@ import Body from "./Body"; //we'll be using these later
 function App() {
   return (
     <div class="box">
-    <div class="row header">
-      <img src={logo} className="App-logo" alt="logo" style={{ width: "30%", height: "30%" }}/>
-      <button>Home</button>
+      <div class="row-header">
+        <div class="logo">
+          <img src={logo} alt="Sarcasm Brewing Logo" style={{ height: "30%" }}/>
+          <p class="logo-text">
+          Sarcastic Brewing
+          </p>
+        </div>
+        <div class="header-buttons">
+          <button class="header-button">Home</button>
+          <button class="header-button">Products</button>
+          <button class="header-button">Merch</button>
+          <button class="header-button">Contact Us</button>
+          <button class="header-button">About Us</button>
+          <button class="header-button">Find a Retailer</button>
+        </div>
+        <div class="social-buttons">
+          <button class="social-button"><img src={iglogo} alt="Instagram Logo" style={{ height: "25px" , marginRight: "20px", marginLeft: "40px"}}/></button>
+          <button class="social-button"><img src={tiktoklogo} alt="Tik Tok Logo" style={{ height: "25px" }}/></button>
+        </div>
+      </div>
+      <div class="row-content">
+        <p>
+        <b>BODY</b>
+        </p>
+      </div>
+      <div class="row-footer">
+        <p><b>FOOTER</b></p>
+      </div>
     </div>
-    <div class="row content">
-      <p>
-      < b>content</b>
-        (fills remaining space)
-      </p>
-    </div>
-    <div class="row footer">
-      <p><b>footer</b> (fixed height)</p>
-    </div>
-</div>
   );
 }
 
